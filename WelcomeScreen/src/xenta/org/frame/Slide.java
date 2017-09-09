01b83595506cf2d288a1c4172b5d3a752e4ae70c
andiroot <andiroot1996@gmail.com>
2017-09-09 10:10:03 +0700

    nambahin animasi,text, dan logo

--- WelcomeScreen/src/xenta/org/frame/Slide.java
+++ WelcomeScreen/src/xenta/org/frame/Slide.java
@@ -5,16 +5,65 @@
  */
 package xenta.org.frame;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+
 /**
  *
  * @author andi
  */
-public class Slide {
+public class Slide extends JLabel{
     String gambar[]=new String[]{
-        "../img/s.jpeg",
-        "../img/Xentaos.jpg",
-        "../img/njay.jpeg"
+        "../img/SLIDE1.png",
+        "../img/SLIDE2.png",
+        "../img/SLIDE3.png",
+        "../img/SLIDE4.png",
+        "../img/SLIDE5.png"
             //isi URL gambar2 disini.. pixel harus sama.
+    };
+    String paragraf[]=new String[]{
+        "<html>"
+            + "<p>"
+            + "Selamat Datang\n" +
+"Xenta OS adalah Distro Linux Lokal Indonesia dikembangkan oleh\n" +
+"Organisasi non-profit. Yang Bertujuan Sebagai Alternatif Sistem \n" +
+"Operasi Bebas, Legal dan Gratis."
+            + "</p>"
+            + "</html>",
+        "<html>"
+            + "<p>"
+            + "Fitur Baru<br>\n" +
+"* Perbaikan Bugs dan Perapihan Paket-Paket.<br>\n" +
+"* Edisi LTS dukungan panjang sampai tahun 2021.<br>\n" +
+"* Merapihkan Aplikasi Bawan dan Perbaikan Theme.<br>"
+            + "</p>"
+            + "</html>",
+        "<html>"
+            + "<p>"
+            + "Komunitas\n" +
+"Xenta OS memiliki wadah Untuk Berkomunikasi dan Berdiskusi \n" +
+"Seputar Linux & Pemecahan Masalah Hardware / Software.\n" +
+"Berkomunitas Facebook Group & Telegram Group. "
+            + "</p>"
+            + "</html>",
+        "<html>"
+            + "<p>"
+            + "Proyek\n" +
+"Xenta OS sebuah proyek kerja secara terbuka,\n" +
+"yang dikerjakan serius berdasarkan hobi.\n" +
+"Lengkapnya Kunjungi http://dev.xentaos.org/ ."
+            + "</p>"
+            + "</html>",
+        "<html>"
+            + "<p>"
+            + "Kontribusi\n" +
+"Xenta OS terbuka bagi siapapun untuk bergabung \n" +
+"mengembangkan, berkreasi dan berinovasi dalam sistem operasi.\n" +
+"Lengkapnya Kunjungi http://dev.xentaos.org/p/join.html/ ."
+            + "</p>"
+            + "</html>"
+        
     };
     int next=0;
     public void setNext(int next){
@@ -27,6 +76,14 @@ public class Slide {
         
         return this.gambar[next];
     }
+    public String setParagraf(){
+        return this.paragraf[next];
+    }
+    
+    
+
+   
+    
     
     
 }
